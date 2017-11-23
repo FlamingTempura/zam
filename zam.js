dad18e067643822115481603f535a5f91c360895
Peter West <peter.g.west@gmail.com>
2017-11-23 21:12:47 +0000

    bump version

--- zam.js
+++ zam.js
@@ -1297,7 +1297,7 @@
 			u.directives.splice(-1 === n ? u.directives.length : n, 0, e);return e
 		};
 	Object.assign(y, {
-		version: "0.6.3",
+		version: "0.6.4",
 		directive: g,
 		__evaluate: o,
 		__parse: s,
